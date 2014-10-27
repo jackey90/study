7064e811305fc3152f968a1908c16f31911c7077
Jackey90 <jackey90.hj@gmail.com>
2014-10-27 15:51:02 +0800

    first commit

---
+++ src/main/java/org/jackey/study/App.java
@@ -0,0 +1,13 @@
+package org.jackey.study;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
