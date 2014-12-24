b5374aa68c5bb09a50812ab50cec0e7c414e9272
Jackey90 <jackey90.hj@gmail.com>
2014-12-24 17:41:25 +0800

    right answer

--- src/main/java/org/jackey/study/algorithm/chapter/four/TheLostInteger.java
+++ src/main/java/org/jackey/study/algorithm/chapter/four/TheLostInteger.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import org.jackey.kingbox.jtool.inAndOut.JOutPrint;
 
 public class TheLostInteger {
-	private static int maxIndex = 100;
+	private static int maxIndex = 20;
 	private static Random r = new Random();
 	static int[] arrayFull = new int[maxIndex];
 	static int[] arrayLost = new int[maxIndex - 1];
@@ -45,57 +45,95 @@ public class TheLostInteger {
 	public static int binaryFind() {
 		int result = -1;
 		int length = arrayFull.length;
-		int maxPos = Integer.bitCount(arrayFull[length - 1]);
+		int maxPos = Integer.toBinaryString(arrayFull[length - 1]).length() - 1;
 		int[] ones = new int[length];
 		int[] zeros = new int[length];
-		int[] indexs = new int[length];
+		int index = 0;
+		// for (int i = length - 1; i >= 0; i--) {
+		// ones[index++] = i;
+		// }
+
 		for (int i = 0; i < length; i++) {
-			indexs[i] = i;
+			ones[i] = i;
 		}
+
 		int count = length;
-		for (int i = maxPos; i >= 0; i--) {
-			count = countOneAndZero(indexs, ones, zeros, i, count);
-			if (count == 0) {
-				result = arrayFull[indexs[0]];
-			}
-		}
+		boolean isOne = true;
+		// for (int i = maxPos; i >= 0; i--) {
+		// count = countOneAndZero(ones, zeros, i, count, isOne);
+		// if (count == 0) {
+		// if (isOne) {
+		// result = arrayFull[ones[0]];
+		// } else {
+		// result = arrayFull[zeros[0]];
+		// }
+		// break;
+		// }
+		// }
+
+		result = countOneAndZero(ones, zeros, maxPos, count, count - 1, isOne);
 
 		return result;
 	}
 
-	public static int countOneAndZero(int[] indexs, int[] ones, int[] zeros,
-			int pos, int count) {
+	public static int countOneAndZero(int[] ones, int[] zeros, int pos,
+			int fullCount, int lostCount, boolean isOne) {
+		if (pos < 0 || fullCount <= 0) {
+			if (isOne) {
+				return arrayFull[ones[0]];
+			} else {
+				return arrayFull[zeros[0]];
+			}
+		}
 
 		int one = 0;
 		int zero = 0;
 		int lostZero = 0;
+		int lostOne = 0;
 		int num1;
 		int num2;
-		for (int i = count - 1; i >= 0; i--) {
-			num1 = arrayFull[indexs[i]];
-			JOutPrint.JPrint(Integer.toBinaryString(num1));
+		int[] indexs;
+		if (isOne) {
+			indexs = ones;
+		} else {
+			indexs = zeros;
+		}
+
+		int index;
+		for (int i = 0; i < fullCount; i++) {
+			index = indexs[i];
+			num1 = arrayFull[index];
+			JOutPrint.JPrint(Integer.toBinaryString(num1) + " num1  pos " + pos
+					+ "    " + num1);
 			if (getTheBitOf(num1, pos) == 0) {
 				zero++;
-				zeros[zero - 1] = i;
+				zeros[zero - 1] = index;
 			} else {
-				ones[one++] = i;
+				ones[one++] = index;
 			}
 
-			if (i < count - 1) {
-				num2 = arrayLost[indexs[i]];
-				JOutPrint.JPrint(Integer.toBinaryString(num2));
+			if (i < lostCount) {
+				num2 = arrayLost[index];
+				JOutPrint.JPrint(Integer.toBinaryString(num2) + " num2 pos "
+						+ pos + "    " + num2);
 				if (getTheBitOf(num2, pos) == 0) {
 					lostZero++;
+				} else {
+					lostOne++;
 				}
 			}
 		}
 
 		if (zero == lostZero) {
-			indexs = ones;
-			return one - 1;
+			JOutPrint.JPrint("one " + one);
+			isOne = true;
+			return countOneAndZero(ones, zeros, pos - 1, one , lostOne ,
+					isOne);
 		} else {
-			indexs = zeros;
-			return zero - 1;
+			JOutPrint.JPrint("zero " + zero);
+			isOne = false;
+			return countOneAndZero(ones, zeros, pos - 1, zero ,
+					lostZero , isOne);
 		}
 
 	}
@@ -106,6 +144,8 @@ public class TheLostInteger {
 
 	public static void main(String[] args) {
 		// JOutPrint.JPrint(foolFind());
+		//JOutPrint.JPrint(binaryFind());
 		JOutPrint.JPrint(binaryFind());
+		
 	}
 }
